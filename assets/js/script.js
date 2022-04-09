efd7f8797c9e30236f079b4a024a1d78dc24c503
TheotherMrUsername <danielwraight067@gmail.com>
2022-04-09 10:01:15 +0000

    no functionality in gitpod

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,12 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 */
 
-
-//pick 5 colors
-
-
-
-//assign them to an Array
+let gameStart = 
+//pick 5 colors assign them to an Array
 
 
 //randomise the array 
+
 function shuffle(array) {
     cardArray.sort(() => Math.random() - 0.5);
 }
@@ -43,21 +40,32 @@ let pair4 = cardArray[3]
 let pair5 = cardArray[4]
 
 //append.css(random) wait 4seconds 
-cardArray[0];
+//
+let pairArray = [pair1, pair2, pair3, pair4, pair5];
 
 
-function assignColor(){
+//want to do a loop in a loop so that the divs with the calss cards have pair1 assigned twice before moviung on to the next pair. 
+for pairs in pairArray{
+    let i = 0 if i < 2 i++
+    $('div').document.getelementsbyClassName('card');
 
 }
+
+
+//function assignColor(){
+
+//}
 
 //function changeCards() {
  ///   let changeCards = document.getElementsByClassName('card').style.borderColor = "red";
 //}
 
 
+//let cardElements = document.getElementsByClassName("card")
+//for(let i = 0; i < cardArray.length; i++) {cardElementsArray[i].addEventListener("click", displayCard)}
 
 
-let card = document.getElementById("card");
+let card = document.getElementById("grid>div");
 
 function changeBorder() {
     card.style.borderColor = "red";
@@ -66,7 +74,7 @@ function changeBorder() {
 function changeBackground() {
     card.style.backgroundColor = ("blue");
 }
-/*
+
 function revertBack() {
     card.style.borderColor = "white";
     card.style.backgroundColor = "darkgreen";
@@ -75,23 +83,24 @@ function revertBack() {
 card.addEventListener('click', changeBorder);
 card.addEventListener('click', changeBackground);
 card.addEventListener('mouseleave', revertBack);
-*/
 
 
-//reset colors  
-let change = document.getElementsByClassName("card");
-function cardClicked(event) {
-        if (this.style.backgroundColor === "darkgreen"){
-            this.style.backgroundColor = "orange";
-            this.style.borderColor = "red";
-        } else {
-            this.style.backgroundColor = "darkgreen";
-            this.style.borderColor = "white"
-        }
-}
-for (i = 0; i < change.length; i++){
-    change[i].addEventListener("click", cardClicked);
-}
+
+//reset colors 
+//** */  
+//let change = document.getElementsByClassName("card");
+//function cardClicked(event) {
+//        if (this.style.backgroundColor === "darkgreen"){
+//            this.style.backgroundColor = "orange";
+ //           this.style.borderColor = "red";
+   //     } else {
+   //         this.style.backgroundColor = "darkgreen";
+   //         this.style.borderColor = "white";
+    //    }
+//}
+//for (i = 0; i < change.length; i++){
+//    change[i].addEventListener("click", cardClicked);
+//} */ 
 
 
 //if the class matches the card clicked 
